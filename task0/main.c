b4b8268a84fcd6fa9f85e8eabab47babf4fe1a09
samsonovak8 <89536093+samsonovak8@users.noreply.github.com>
2021-08-25 21:00:25 +0300

    new_function

--- task0/main.c
+++ task0/main.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int check_data(int a, int b, int c)
+{
+    if ((a == 0) && (b == 0) && (c != 0))
+        return 0;
+    double d = b * b - 4 * a * c;
+    if (d < 0)
+        return 0;
+    return 1;
+}
 int main()
 {
     double a = 0, b = 0, c = 0;
     printf("Enter quadratic coefficients: a, b, c\n");
     scanf("%lf%lf%lf", &a, &b, &c);
-    double D = b * b - 4 * a * c;
-    if (D < 0)
+    if (!check_data(a, b, c))
         printf("No roots\n");
     else
     {
-        if (D == 0)
-        {
-            double x1 = (b * (-1)+sqrt(D))/(2 * a);
-            printf("One root: x = %lf\n", x1);
-        }
-        else
-        {
-            double x1 = (b * (-1) - sqrt(D))/(2 * a);
-            double x2 = (b * (-1) + sqrt(D))/(2 * a);
-            printf("Two roots: x1 = %lf, x2 = %lf\n", x1, x2);
-        }
+        double D = b * b - 4 * a * c;
+        double x1 = (b * (-1) - sqrt(D))/(2 * a);
+        double x2 = (b * (-1) + sqrt(D))/(2 * a);
+        if (x1 == x2)
+            printf("One root: x = %lf", x1);
+        else 
+            printf("Two roots: x1 = %lf, x2 = %lf", x1, x2);
     }
     return 0;
 }
