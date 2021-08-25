9abfa969f4bbd837de621a6ae12c7294cf6b3299
samsonovak8 <89536093+samsonovak8@users.noreply.github.com>
2021-08-25 20:28:34 +0300

    visual changes
    initialization+dividing+some spaces

--- task0/main.c
+++ task0/main.c
@@ -3,24 +3,24 @@
 
 int main()
 {
-    double a, b, c, D, x1, x2;
+    double a = 0, b = 0, c = 0;
     printf("Enter quadratic coefficients: a, b, c\n");
     scanf("%lf%lf%lf", &a, &b, &c);
-    D=b*b-4*a*c;
-    if (D<0)
+    double D = b * b - 4 * a * c;
+    if (D < 0)
         printf("No roots\n");
     else
     {
-        if (D==0)
+        if (D == 0)
         {
-            x1=(b*(-1)+sqrt(D))/(2*a);
-            printf("One root: x=%lf\n", x1);
+            double x1 = (b * (-1)+sqrt(D))/(2 * a);
+            printf("One root: x = %lf\n", x1);
         }
         else
         {
-            x1=(b*(-1)-sqrt(D))/(2*a);
-            x2=(b*(-1)+sqrt(D))/(2*a);
-            printf("Two roots: x1=%lf, x2=%lf\n", x1, x2);
+            double x1 = (b * (-1) - sqrt(D))/(2 * a);
+            double x2 = (b * (-1) + sqrt(D))/(2 * a);
+            printf("Two roots: x1 = %lf, x2 = %lf\n", x1, x2);
         }
     }
     return 0;
