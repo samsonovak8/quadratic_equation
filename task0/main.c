f5465f978932f165698ea6f1452846356e911adc
samsonovak8 <89536093+samsonovak8@users.noreply.github.com>
2021-08-26 19:29:50 +0300

    divided on functions
    code was divided on functions

--- task0/main.c
+++ task0/main.c
@@ -1,31 +1,88 @@
+//------------------------------------------------
+//! Solves a square equation ax^2 + bx + c = 0
+//!
+//! @param [in] a a-coefficient
+//! @param [in] b b-coefficient
+//! @param [in] c c-coefficient
+//! @param [out] x1 Pointer to the 1st root
+//! @param [out] x2 Pointer to the 2nd root
+//!
+//! @return Number of roots
+//!
+//! @note In case of infinite number of roots,
+//! returns SS_INF_ROOTS.
+//------------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <assert.h>
 
-int check_data(int a, int b, int c)
+
+double find_diskriminant(double a, double b, double c)
 {
-    if ((a == 0) && (b == 0) && (c != 0))
-        return 0;
     double d = b * b - 4 * a * c;
-    if (d < 0)
-        return 0;
-    return 1;
+    return d;
+}
+int solveEquation(double a, double b, double c, double* x1, double* x2)
+{
+    assert(isfinite(a));
+    assert(isfinite(b));
+    assert(isfinite(c));
+
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    assert(x1 != x2);
+
+    if (a == 0)
+    {
+        if (b == 0)
+        {
+            return (c == 0) ? 3:0;
+        }
+        else //if (b != 0)
+        {
+            *x1 = (-c)/b;
+            return 1;
+        }
+    }
+    else //if (a != 0)
+    {
+        double D = find_diskriminant(a, b, c);
+        if (D < 0)
+            return 0;
+        if (D == 0)
+        {
+            *x1 = *x2 = (-b)/(2*a);
+            return 1;
+        }
+        else //if (D > 0)
+        {
+            *x1 = (- b - sqrt(D))/(2 * a);
+            *x2 = (- b + sqrt(D))/(2 * a);
+            return 2;
+        }
+    }
 }
 int main()
 {
     double a = 0, b = 0, c = 0;
     printf("Enter quadratic coefficients: a, b, c\n");
     scanf("%lf%lf%lf", &a, &b, &c);
-    if (!check_data(a, b, c))
-        printf("No roots\n");
-    else
+    double x1 = 0, x2 = 0;
+    int number_roots = solveEquation(a, b, c, &x1, &x2);
+    switch (number_roots)
     {
-        double D = b * b - 4 * a * c;
-        double x1 = (b * (-1) - sqrt(D))/(2 * a);
-        double x2 = (b * (-1) + sqrt(D))/(2 * a);
-        if (x1 == x2)
-            printf("One root: x = %lf", x1);
-        else 
-            printf("Two roots: x1 = %lf, x2 = %lf", x1, x2);
+    case 0: printf("No roots\n");
+            break;
+    case 1: printf("One root: x1 = %lf", x1);
+            break;
+    case 2: printf("Two roots: x1 = %lf, x2 = %lf", x1, x2);
+            break;
+    case 3: printf("Any number\n");
+            break;
+    default: printf("main(): EROOR number_roots = %d", number_roots);
+            return 1;
     }
     return 0;
 }
